0f6d7f67837f979d007d0d1f1fa8bd400b5bac49
cho00054 <gonQ321!>
2018-02-17 18:42:09 -0500

    lab5

--- app/src/main/java/com/example/cho00054/androidlabs/ChatWindow.java
+++ app/src/main/java/com/example/cho00054/androidlabs/ChatWindow.java
@@ -1,9 +1,13 @@
 package com.example.cho00054.androidlabs;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,34 +15,82 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ChatWindow extends Activity {
-    ArrayList<String> list;
+    private ArrayList<String> list;
+    private Context ctx;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+        ctx = this;
 
         ListView listView = findViewById(R.id.chatView);
-        EditText textInput = findViewById(R.id.chatViewText);
         Button sendButton = findViewById(R.id.sendButton);
         list = new ArrayList<>();
-
-        ChatAdapter messageAdapter = new ChatAdapter( this );
+        ChatAdapter messageAdapter = new ChatAdapter( ctx );
         listView.setAdapter(messageAdapter);
+
+        ChatDatabaseHelper dbHelper = new ChatDatabaseHelper(ctx);
+        db = dbHelper.getWritableDatabase();
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText textInput = findViewById(R.id.chatViewText);
                 list.add(textInput.getText().toString());
                 messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount() & getView()
                 textInput.setText("");
+
+                ContentValues newData = new ContentValues();
+                newData.put(ChatDatabaseHelper.KEY_MESSAGE, textInput.getText().toString());
+
+                db.insert(ChatDatabaseHelper.TABLE_NAME, null, newData);
+
+                Cursor results = db.query(false, ChatDatabaseHelper.TABLE_NAME,
+                        new String[] {ChatDatabaseHelper.KEY_ID, ChatDatabaseHelper.KEY_MESSAGE},
+                        null, null,
+                        null, null, null, null);
+
+                results.moveToFirst();
+                while(!results.isAfterLast()){
+                    String resultMessage = results.getString(results.getColumnIndex(ChatDatabaseHelper.KEY_MESSAGE));
+                    Log.i("ChatWindow", "SQL MESSAGE:" + resultMessage);
+                    results.moveToNext();
+                }
+
+                Log.i("ChatWindow", "Cursor’s  column count =" + results.getColumnCount());
+                for(int i = 0; i < results.getColumnCount(); i++) {
+                    Log.i("ChatWindow", "Cursor’s  column name =" + results.getColumnName(i));
+                }
+
+                results.moveToFirst();  //reset the cursor
+
+                try {
+                    SimpleCursorAdapter listAdapter =
+                            new SimpleCursorAdapter(ctx, R.layout.activity_chat_window, results,
+                                    new String[]{ChatDatabaseHelper.KEY_MESSAGE},
+                                    new int[]{R.id.chatView}, 0);
+
+                    listView.setAdapter(listAdapter); //populate the list with results\}
+                }catch(Exception e)
+                {
+                    Log.e("Crash!", e.getMessage());
+                }
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
     }
 
     private class ChatAdapter extends ArrayAdapter<String>{
